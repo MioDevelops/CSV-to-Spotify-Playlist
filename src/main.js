9e901011dc44a75c8ce1e399cb650239b6e85f71
Hunter <60201971+HooferDevelops@users.noreply.github.com>
2023-02-01 18:26:30 -0500

    base

--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
-const axios = require("axios").default
+const TESTS = require("./tests");
+const TESTS_INSTANCE = new TESTS();
 
-async function main() {
-	const res = await axios.post("https://api.spotify.com/v1/playlists/7jgHxPffuBMeEZFtWwrJqb/tracks?uris=spotify:track:01WJWHv12F3nWeXGYsmOOs", {}, {
-		headers: {
-			"Accept": "application/json",
-			"Content-Type": "application/json",
-			"Authorization": "Bearer BQC-u5_o-lEk424-dO8KQxPvdEptxfW7tTGAajWmSnMOSI7JAfqEfzGkCHhwgEkvbtoO4zozXtAJM8zQZ4Unh1q2NL9RtuBknF7NljovBdZ9eUS_jJN4sKzPFaddGR658y0irj5uRnCVJcySVyCXANXawLMjDHraq6GxQCz5ftBS3UXlk9Rjby4Uk3Cfcf8U8WJdYNeCYcuknkHih5gypBfjrvEyf-7MQxQdmialzQKIe2dC9w-VvYeAXlXRjFZ9CA"
-		}
-	})
-	console.log(res.body)
-}
-
-main()
+TESTS_INSTANCE.get();
