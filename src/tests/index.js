e7501b7d3a26e884df61ebc1306b6bcf040983b8
MioDevelops <94136291+MioDevelops@users.noreply.github.com>
2023-02-01 20:02:09 -0500

    little debug changes

--- src/tests/index.js
+++ src/tests/index.js
@@ -17,13 +17,13 @@ class Tests {
     async get_track(track_id, market_type) {
         this.axios.get(`https://api.spotify.com/v1/tracks/${track_id}?market=${market_type}`, {
             headers: this.header
-        }).then((response) => console.log(response.data)).catch((err) => console.log("Failed to get track"))
+        }).then((response) => console.log(response.data)).catch((err) => console.error("Failed to get track with an error code of", err.response.status))
     }
 
     async add_tracks(playlist_id, track_list) {
         this.axios.post(`https://api.spotify.com/v1/playlists/${playlist_id}/tracks?uris=${track_list.join("%2C")}`, {}, {
             headers: this.header
-        }).then((response) => console.log("Songs: ", response.statusText)).catch((err) => console.log("Failed to add tracks"))
+        }).then(() => console.log("Successfully added songs to desired playlist!")).catch((err) => console.error("Failed to add tracks with an error code of", err.response.status))
     }
 }
 
